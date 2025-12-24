0d4ccd21324634de397a4f4514cd576fb866e68e
Ibrahim-Rezq <ovic391@gmail.com>
2025-12-24 16:04:49 +0200

    Fix: fix backend bug && feat: add cards to bestSeller section

---
+++ client/src/components/ShopBy.jsx
@@ -0,0 +1,105 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import HorSlider from './HorSlider'
+
+const ShopBy = ({ filter, title }) => {
+    const [products, setProducts] = useState([])
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const copiedData = [
+        { src: '/GenInfo/adidas.jpg', name: 'Adidas', to: '/search/adidas' },
+        { src: '/GenInfo/nike.png', name: 'Nike', to: '/search/nike' },
+        {
+            src: '/GenInfo/skechers.jpg',
+            name: 'Skechers',
+            to: '/search/skechers',
+        },
+        { src: '/GenInfo/puma.jpg', name: 'Puma', to: '/search/puma' },
+    ]
+    useEffect(() => {
+        let isMounted = true
+        const fetchData = async () => {
+            try {
+                const res = await axios.get(
+                    `${import.meta.env.VITE_BASE_URL}/api/filter/${filter}`
+                )
+                if (isMounted) {
+                    setProducts(res.data)
+                    setLoading(false)
+                }
+            } catch (err) {
+                if (isMounted) {
+                    console.error(
+                        `Error while fetching products: ${err.message}`
+                    )
+                    setError(err)
+                    setLoading(false)
+                }
+            }
+        }
+        fetchData()
+        return () => {
+            isMounted = false
+        }
+    }, [])
+    return (
+        <>
+            <div className='mt-10 mb-2 text-2xl'>{title}</div>
+            <div
+                className={`overflow-x-auto overflow-y-hidden md:max-w-full scroll-container mb-10 mx-auto relative scroll-container ${
+                    filter === 'bestSellers' ? 'w-full' : ''
+                }`}
+            >
+                {loading && <p>Loading...</p>}
+                {error && <p>Error while fetching: {error.message}</p>}
+
+                <div
+                    className={`flex ${
+                        filter === 'bestSellers'
+                            ? 'flex-wrap justify-center m-auto'
+                            : 'flex-nowrap space-x-4'
+                    }`}
+                >
+                    {/* Ensure products is always an array */}
+                    {filter === 'bestSellers'
+                        ? copiedData.map((elem, id) => (
+                              <div
+                                  key={id}
+                                  className='relative w-[340px] h-[340px] mx-2 mb-6 hover:text-white'
+                              >
+                                  <div className='absolute w-full flex justify-center items-center top-4  '>
+                                      <p className='logo font-semibold z-50 text-sm'>
+                                          {elem.name}
+                                      </p>
+                                  </div>
+                                  <img
+                                      src={elem.src}
+                                      alt={elem.name}
+                                      className='w-full h-full object-cover m-0'
+                                  />
+                                  <button
+                                      onClick={() => navigate(elem.to)}
+                                      className='absolute inset-0 flex items-center justify-center
+                             bg-gray-800 text-white opacity-0 hover:opacity-80 transition-opacity duration-200 text-sm'
+                                  >
+                                      Explore →
+                                  </button>
+                              </div>
+                          ))
+                        : (Array.isArray(products) ? products : []).map(
+                              (elem) => (
+                                  <HorSlider
+                                      product={elem}
+                                      key={elem._id || elem.id} // fallback if _id is missing
+                                      className='inline-block'
+                                      home={true}
+                                  />
+                              )
+                          )}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ShopBy
---
+++ server/index.js
@@ -0,0 +1,42 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import router from './routes/productRoutes.js'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 5000
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            process.env.MONGO_URI ||
+                'mongodb+srv://ebrahimamin391_db_user:XFHIeyGY4bL72xJn@cluster0.nrfzvb7.mongodb.net/?appName=Cluster0'
+        )
+        console.log(`App is connected to the database.`)
+    } catch (error) {
+        console.error(`Error connecting to DB: ${error.message}`)
+        process.exit(1)
+    }
+}
+
+const startServer = async () => {
+    try {
+        await connectDB()
+        app.listen(PORT, () => {
+            console.log(`App is listening on port ${PORT}`)
+        })
+        app.use('/api', router)
+    } catch (error) {
+        console.error(`Error starting server: ${error.message}`)
+        process.exit(1)
+    }
+}
+startServer()
